4ec9ff56a81925b81e28333b9ced03e1074d8df8
keila valdez <keyvapers@hotmail.com>
2020-04-24 06:00:33 -0500

    Agrega validacion sesion

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -89,7 +89,7 @@ $autoload['drivers'] = array();
 |
 |	$autoload['helper'] = array('url', 'file');
 */
-$autoload['helper'] = array('url','tipos');
+$autoload['helper'] = array('url','tipos','validar');
 
 /*
 | -------------------------------------------------------------------
--- application/controllers/Vehiculos.php
+++ application/controllers/Vehiculos.php
@@ -4,7 +4,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Vehiculos extends CI_Controller {
 
     public function __construct(){
-		parent::__construct();
+        parent::__construct();
+        validarSesion();
     }
 
     public function index(){
