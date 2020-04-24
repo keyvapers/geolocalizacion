636d1cc02e34144b49b4bff806678d3b8caf5ac0
keila valdez <keyvapers@hotmail.com>
2020-04-24 06:12:31 -0500

    Fix variables session en footer

--- application/views/partials/footer_view.php
+++ application/views/partials/footer_view.php
@@ -1,6 +1,18 @@
 </body>
 <footer>
     <span>Keila Valdéz 2020</span>
+    <?php 
+        $user	= [
+            "userId"    => $this->session->userdata('userId'),
+            "username"  => $this->session->userdata('username'),
+            "tipoUsuario"     => $this->session->userdata('tipoUsuario')
+        ];
+    ?>
+    <script>
+        sessionStorage.userData 	= '<?php echo json_encode($user);?>';
+        sessionStorage.baseUrl  	= '<?php echo base_url();?>';
+        localStorage.url_api	= '<?php echo base_url() . $this->config->item('api_url'); ?>';
+    </script>
     <script src="<?php echo base_url('src/js/jquery-3.2.1.min.js')?>"></script>
     <script src="<?php echo base_url('src/js/bootstrap/bootstrap.min.js'); ?>"></script>
     <script src="<?php echo base_url('src/js/jquery.validate.min.js')?>"></script>
@@ -12,16 +24,8 @@
                 $url    = base_url("src/js/$url.js");
                 echo "<script src='$url'></script>";
             }
-            $user	= [
-                        "userId"    => $this->session->userdata('userId'),
-                        "username"  => $this->session->userdata('username'),
-                        "tipoUsuario"     => $this->session->userdata('tipoUsuario')
-                    ];
+           
     ?>
-    <script>
-        sessionStorage.userData 	= '<?php echo json_encode($user);?>';
-        sessionStorage.baseUrl  	= '<?php echo base_url();?>';
-        localStorage.url_api	= '<?php echo base_url() . $this->config->item('api_url'); ?>';
-    </script>
+    
 </footer>
 </html>
